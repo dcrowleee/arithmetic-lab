ff9d28abb0d2f2737697cf72fb1e989536df3f76
Dan Crowley <dancr.wley@gmail.com>
2023-01-18 18:28:36 -0600

    calc complete

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(x, y) {
+    return x + y;
+    console.log(add());
+}
+
+function subtract(x, y) {
+    return x - y;
+    console.log(subtract());
+}
+
+function multiply(x ,y) {
+    return x * y;
+    console.log(multiply());
+}
+
+function divide(x ,y) {
+    return x / y;
+    console.log(divide());
+}
+
+function increment(n) {
+    return n + 1 ;
+    console.log(increment());
+}
+
+function decrement(n) {
+    return n - 1 ;
+    console.log(decrement());
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+    console.log(makeInt());
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+    console.log(preserveDecimal())
+}
